6ebac187f0e48191e0a5c83808badf4d23c83e33
Jesus <jesus.peralesmr@uanl.edu.mx>
2014-10-23 00:55:27 -0500

    set enviromen variables

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ var ipaddr = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_I
 
 //Twitter configuration
 var twit = new twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 //Busca y almacena tuits por geolocalizacion en tiempo real
